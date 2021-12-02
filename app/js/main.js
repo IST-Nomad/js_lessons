2092c15f20c3ec345509d515b730efdecd4ed485
Batr <istnomad@yandex.ru>
2021-12-02 21:59:04 +0800

    02.12

--- app/js/main.js
+++ app/js/main.js
@@ -1,110 +1,47 @@
-// 1. Создать объект, который описывает ширину и высоту
-// прямоугольника, а также может посчитать площадь фигуры:
-// const rectangle = {width:..., height:..., getSquare:...};
+const plus = (x, y) => x + y;
+const plusRes = plus(1, 2);
+console.log(plusRes);
 
-const rectangle = {
-  width: 4,
-  height: 4,
-  getSquare: function getSquare() {
-    return parseFloat(this.width) * parseFloat(this.height);
-  },
-};
-console.log(rectangle.getSquare());
-
-// 2. Создать объект, у которого будет цена товара и его скидка, а также
-// два метода: для получения цены и для расчета цены с учетом скидки:
-// const price = {
-//     price: 10,
-//     discount: '15%',
-// ... };
-// price.getPrice(); // 10
-// price.getPriceWithDiscount(); // 8.5
-
-const price = {
-  price: 10,
-  discount: "15%",
-  getPrice: function getPrice() {
-    return parseFloat(this.price);
-  },
-  getPriceWithDiscount: function getPriceWithDiscount() {
-    return (
-      parseFloat(this.price) -
-      (parseFloat(this.price) * parseFloat(this.discount)) / 100
-    );
-  },
-};
-price.getPrice(); // 10
-price.getPriceWithDiscount(); // 8.5
-
-// 3. Создать объект, у которого будет поле высота и метод “увеличить
-// высоту на один”. Метод должен возвращать новую высоту:
-// object.height = 10;
-// object.inc(); // придумать свое название для метода
-// object.height; // 11;
-
-const mult = {
-  height: 10,
-  mag: function magMult() {
-    return this.height + 1;
-  },
-};
-console.log(mult.height);
-mult.mag();
-
-// 4. Создать объект “вычислитель”, у которого есть числовое свойство
-// “значение” и методы “удвоить”, “прибавить один”, “отнять один”.
-// Методы можно вызывать через точку, образуя цепочку методов:
-// const numerator = {
-//     value: 1,
-//     double: function () {...},
-//     plusOne: function () {...},
-//     minusOne: function () {...},
-// }
-// numerator.double().plusOne().plusOne().minusOne();
-// numerator.value // 3
-
-const numerator = {
-  value: 1,
-  double: function () {
-    this.value *= 2;
-    return this;
-  },
-  plusOne: function () {
-    this.value++;
-    return this;
-  },
-  minusOne: function minusOne() {
-    this.value--;
-    return this;
-  },
+function plusFoo(x, y) {
+    return x + y;
 };
 
-numerator.double().plusOne().plusOne().minusOne();
-console.log(numerator.value);
+const withoutArgs = () => console.log('Hello world!');
+const singleArgs = x => x*2;
 
-// 5. Создать объект с розничной ценой и количеством продуктов. Этот
-// объект должен содержать метод для получения общей стоимости
-// всех товаров (цена * количество продуктов)
-
-const mag = {
-  price: 5,
-  count: 6,
-  summ: function summ() {
-    this.price * this.count;
-    return this;
-  },
-};
-console.log(mag.summ);
-
-// 6. Создать объект из предыдущей задачи. Создать второй объект,
-// который описывает количество деталей и цену за одну деталь. Для
-// второго объекта нужно узнать общую стоимость всех деталей, но
-// нельзя создавать новые функции и методы. Для этого
-// “позаимствуйте” метод из предыдущего объекта.
-
-const detail = {
-  price: 4,
-  count: 3,
+const moreActions = (a, b) => {
+    a *=2;
+    b *=3;
+    return a + b;
 };
 
-// console.log(summ.call(detail));
+const returnObj = (str = '') => ({
+    value: str,
+    length: str.length,
+});
+
+// 1. Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
+
+  
+const sum = (...allArguments) => {
+ 
+    if (!allArguments.length) return 0;
+ 
+    return allArguments.reduce((prev, next) => prev + next);
+}
+ 
+console.log(sum());
+console.log(sum(1, 2, 3, 4));
+
+
+// 2. Переделать функцию с использованием функции-стрелки
+
+const convertToObject = (num) => ({
+      value: num,
+
+      isOdd: Boolean(num % 2)
+
+  });
+
+
+
